0b9ef408b518de44f81a305e68ba5647cb5e4aaf
Osomudeya Zudonu <talk2osomudeya@gmail.com>
2023-01-25 22:30:14 +0100

    Update backend-services.tf

--- terraform-aws-vprofile-project/backend-services.tf
+++ terraform-aws-vprofile-project/backend-services.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "vprofile-rds" {
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "mysql"
-  engine_version         = "5.7.33"
+  engine_version         = "5.7.34"
   instance_class         = "db.t2.micro"
   name                   = var.db_name
   username               = var.dbuser
