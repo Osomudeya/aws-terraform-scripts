4392f0facb1971b214eebb2f6034fc87cee5cbbf
Osomudeya Zudonu <talk2osomudeya@gmail.com>
2023-01-26 10:20:31 +0100

    Update backend-services.tf

--- terraform-aws-vprofile-project/backend-services.tf
+++ terraform-aws-vprofile-project/backend-services.tf
@@ -1,14 +1,17 @@
 resource "aws_db_subnet_group" "vprofile-rds-subgrp" {
-  name       = "main"
+  name       = "vprofile-rds-subgrp"
   subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
   tags = {
-    Name = "Subnet group for RDS"
+    Name = "Subnet groups for RDS"
   }
 }
 
 resource "aws_elasticache_subnet_group" "vprofile-ecache-subgrp" {
   name       = "vprofile-ecache-subgrp"
   subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  tags = {
+    Name = "Subnet groups for ECACHE"
+  }
 
 }
 
@@ -18,13 +21,13 @@ resource "aws_db_instance" "vprofile-rds" {
   engine                 = "mysql"
   engine_version         = "5.7.34"
   instance_class         = "db.t2.micro"
-  name                   = var.db_name
+  db_name                = var.dbname
   username               = var.dbuser
   password               = var.dbpass
   parameter_group_name   = "default.mysql5.7"
-  multi_az               = "false"
-  publicly_accessible    = "false"
   skip_final_snapshot    = true
+  publicly_accessible    = false
+  multi_az               = false
   db_subnet_group_name   = aws_db_subnet_group.vprofile-rds-subgrp.name
   vpc_security_group_ids = [aws_security_group.vprofile-backend-sg.id]
 }
@@ -36,8 +39,8 @@ resource "aws_elasticache_cluster" "vprofile-cache" {
   num_cache_nodes      = 1
   parameter_group_name = "default.memcached1.6"
   port                 = 11211
-  security_group_ids   = [aws_security_group.vprofile-backend-sg.id]
   subnet_group_name    = aws_elasticache_subnet_group.vprofile-ecache-subgrp.name
+  security_group_ids   = [aws_security_group.vprofile-backend-sg.id]
 }
 
 resource "aws_mq_broker" "vprofile-rmq" {
