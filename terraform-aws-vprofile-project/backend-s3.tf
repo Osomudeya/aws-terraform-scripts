317069d3fd896e8de86530cc56affddf10a2d81c
Osomudeya Zudonu <talk2osomudeya@gmail.com>
2023-01-26 21:52:24 +0100

    Update backend-s3.tf

--- terraform-aws-vprofile-project/backend-s3.tf
+++ terraform-aws-vprofile-project/backend-s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terra-state-1993"
-    key    = "terraform/backend"
-    region = "us-east-2"
+    bucket = "terra-state-vprofile-1993"
+    key    = "terraform"
+    region = "us-east-1"
   }
 }
