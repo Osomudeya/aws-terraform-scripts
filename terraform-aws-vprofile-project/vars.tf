3f5fd6704a23297c5c02085b78e8b0e8cdeb5c0a
Osomudeya Zudonu <talk2osomudeya@gmail.com>
2023-01-26 23:40:25 +0100

    Update vars.tf

--- terraform-aws-vprofile-project/vars.tf
+++ terraform-aws-vprofile-project/vars.tf
@@ -5,7 +5,7 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(any)
   default = {
-    us-east-1 = "ami-0778521d914d23bc1" # ubuntu 20.04 AMI
+    us-east-2 = "ami-00149760ce42c967b" # ubuntu 20.04 AMI
     us-east-2 = "ami-0574da009dca65348"
     us-west-1 = "ami-0574da719dca65125"
   }
