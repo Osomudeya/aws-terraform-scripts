db443e6272a993e1c35c2a8a73020219521b4166
Osomudeya Zudonu <talk2osomudeya@gmail.com>
2023-01-27 01:59:40 +0100

    Update bean-app.tf

--- terraform-aws-vprofile-project/bean-app.tf
+++ terraform-aws-vprofile-project/bean-app.tf
@@ -1,3 +1,29 @@
 resource "aws_elastic_beanstalk_application" "vprofile-prod" {
   name = "vprofile-prod"
 }
+
+}
+
+resource "aws_iam_role" "aws-elasticbeanstalk-ec2-role" {
+  name = "aws-elasticbeanstalk-ec2-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
+  name = "vprofile-bean-prod"
+  # ...
+  instance_profile_arn = aws_iam_role.aws-elasticbeanstalk-ec2-role.arn
+}
